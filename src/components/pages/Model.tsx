25bba6dfb89e4d91a5469d7f599d54ded9b8c2e2
alekcandr1 <alekcandrmain@gmail.com>
2024-06-08 12:02:46 +0300

    useParams refactoring

---
+++ src/components/pages/Model.tsx
@@ -0,0 +1,38 @@
+// @flow
+import * as React from 'react';
+import { useParams } from 'react-router-dom';
+import { adidasArr, ModelItem } from './Adidas';
+import { pumaArr } from './Puma';
+
+type ModelProps = {};
+
+
+type allModelsType = {
+    [key: string]: ModelItem[]
+}
+
+const allModels: allModelsType = {
+    adidas: adidasArr,
+    puma: pumaArr
+}
+
+export const Model = ( props: ModelProps ) => {
+
+    const {manufacturer, modelID} = useParams()
+    console.log(manufacturer)
+    console.log(modelID)
+
+    const model = manufacturer
+        ? allModels[manufacturer].find(model => model.id === Number(modelID))
+        : allModels.adidas[0]
+
+    return model === undefined
+        ? <p>Такой модели не существует</p>
+        : <div style={ {paddingLeft: '50px'} }>
+            <h1>{ model.model }</h1>
+            <h3>{ model.collection }</h3>
+            <p>{ model.price }</p>
+            <img src={ model.picture } alt="" style={ {width: '80%'} } />
+        </div>
+    return <div></div>
+};
